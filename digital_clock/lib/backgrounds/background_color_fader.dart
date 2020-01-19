2402e36e15cd1dcdb7c0bc0e403a08e457f05373
Jeff <abc@abc.com>
2020-01-19 18:12:45 -0500

    Randomize background colors every so often.

--- digital_clock/lib/backgrounds/background_color_fader.dart
+++ digital_clock/lib/backgrounds/background_color_fader.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:digital_clock/peg_board.dart';
+import '../peg_board.dart';
+import '../elapsed_time.dart';
 import '../color_fader.dart';
 
 import 'background_manager.dart';
@@ -9,6 +10,11 @@ import 'background_manager.dart';
 class BackgroundColorFader implements IBackground {
   PegBoard pegBoard;
   Random _random;
+  ElapsedTime _elapsedTime;
+
+  Color backgroundColor;
+
+  ColorFader colorFader;
 
   int redDelta = 0;
   int blueDelta = 0;
@@ -18,20 +24,35 @@ class BackgroundColorFader implements IBackground {
   BackgroundColorFader({@required this.pegBoard}) {
     _random = Random(DateTime.now().second);
 
+    _elapsedTime = ElapsedTime(targetMilliseconds: 60000);
+
+    _randomizeColors();
+  }
+
+  _randomizeColors() {
+    backgroundColor = pegBoard.randomColor(0.5);
+
     redDelta = _random.nextInt(12) * (_random.nextInt(7) > 3 ? 1 : -1);
     blueDelta = _random.nextInt(12) * (_random.nextInt(7) > 3 ? 1 : -1);
     greenDelta = _random.nextInt(12) * (_random.nextInt(7) > 3 ? 1 : -1);
+
+    colorFader = ColorFader(startColor: backgroundColor, redDelta: redDelta, blueDelta: blueDelta, greenDelta: greenDelta);
   }
 
   void draw() {
-    final colorFader = ColorFader(startColor: pegBoard.globalBackgroundColor, redDelta: redDelta, blueDelta: blueDelta, greenDelta: greenDelta);
+    if (_elapsedTime.timesUp()) {
+      _randomizeColors();
+      _elapsedTime.reset();
+    }
 
     int leftPegId = pegBoard.pegId(0, 0);
     int curPegId = leftPegId;
 
     for (int i = 0; i < PegBoard.pegHeight; i++) {
       for (int j = 0; j < PegBoard.pegWidth; j++) {
-        pegBoard.getPeg(curPegId).pegColor = colorFader.color;
+        pegBoard
+            .getPeg(curPegId)
+            .pegColor = colorFader.color;
         curPegId++;
       }
 
