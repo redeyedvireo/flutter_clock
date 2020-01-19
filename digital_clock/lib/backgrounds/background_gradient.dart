d77768d6e0523501d651ea1abeb5515bbaf788ea
Jeff <abc@abc.com>
2020-01-19 18:19:51 -0500

    Randomize starting gradient stop.

--- digital_clock/lib/backgrounds/background_gradient.dart
+++ digital_clock/lib/backgrounds/background_gradient.dart
@@ -1,5 +1,7 @@
-import 'package:digital_clock/peg_board.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import '../peg_board.dart';
 
 import 'background_manager.dart';
 
@@ -8,6 +10,7 @@ import '../color_gradient.dart';
 
 class BackgroundGradient implements IBackground {
   PegBoard pegBoard;
+  Random _random;
 
   ElapsedTime _backgroundUpdateElapsedTime;
 
@@ -18,26 +21,28 @@ class BackgroundGradient implements IBackground {
   int _backgroundGradientSteps = 30;
 
   BackgroundGradient({@required this.pegBoard}) {
-      _currentStop = 0;
-      _currentPositionInStop = 0;
+    _random = Random(DateTime.now().second);
 
-      _backgroundUpdateElapsedTime = ElapsedTime(targetMilliseconds: 5000);
+    _currentStop = _random.nextInt(12);
+    _currentPositionInStop = 0;
 
-      // Color values are from: https://en.wikipedia.org/wiki/RGB_color_model
-      colorGradient = ColorGradient(colorStops: [
-        Color.fromARGB(255, 127, 0, 255),       // Violet
-        Color.fromARGB(255, 0, 0, 255),         // Blue
-        Color.fromARGB(255, 0, 127, 255),       // Azure
-        Color.fromARGB(255, 0, 255, 255),       // Cyan
-        Color.fromARGB(255, 0, 255, 127),       // Spring
-        Color.fromARGB(255, 0, 255, 0),         // Green
-        Color.fromARGB(255, 128, 255, 0),       // Chartreuse
-        Color.fromARGB(255, 255, 255, 0),       // Yellow
-        Color.fromARGB(255, 255, 127, 0),       // Orange
-        Color.fromARGB(255, 255, 0, 0),         // Red
-        Color.fromARGB(255, 255, 0, 127),       // Rose
-        Color.fromARGB(255, 255, 0, 255),       // Magenta
-      ], stepsPerStop: _backgroundGradientSteps);
+    _backgroundUpdateElapsedTime = ElapsedTime(targetMilliseconds: 5000);
+
+    // Color values are from: https://en.wikipedia.org/wiki/RGB_color_model
+    colorGradient = ColorGradient(colorStops: [
+      Color.fromARGB(255, 127, 0, 255),       // Violet
+      Color.fromARGB(255, 0, 0, 255),         // Blue
+      Color.fromARGB(255, 0, 127, 255),       // Azure
+      Color.fromARGB(255, 0, 255, 255),       // Cyan
+      Color.fromARGB(255, 0, 255, 127),       // Spring
+      Color.fromARGB(255, 0, 255, 0),         // Green
+      Color.fromARGB(255, 128, 255, 0),       // Chartreuse
+      Color.fromARGB(255, 255, 255, 0),       // Yellow
+      Color.fromARGB(255, 255, 127, 0),       // Orange
+      Color.fromARGB(255, 255, 0, 0),         // Red
+      Color.fromARGB(255, 255, 0, 127),       // Rose
+      Color.fromARGB(255, 255, 0, 255),       // Magenta
+    ], stepsPerStop: _backgroundGradientSteps);
   }
 
   void draw() {
